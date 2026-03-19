3cb05a47f8f6e00429fdb1cb6bebf54a861b6000
Oliver Kelly <foolishhim@gmail.com>
2026-03-19 14:44:47 +0000

    Lesson 4 - Introduction to python

---
+++ 04-introduction-to-python/main.py
@@ -0,0 +1,49 @@
+# name = "Oliver"
+# age = 40
+# running_speed = 5.2 # MPH
+# is_student = False
+#
+# print(name)
+# print(age)
+# print(running_speed)
+# print(is_student)
+#
+
+# print(type(name))
+# print(type(age))
+# print(type(running_speed))
+# print(type(is_student))
+# print(f"Hello {name} you are {age} years old")
+#
+# if age < 18 :
+#     print("You are a child")
+#
+# elif age > 18 and age < 60:
+#     print("You are a grown up")
+# else:
+#     print("YOU ARE OLD")
+#
+#
+# x = 10
+# y = 10
+# if x > y:
+#     print("x is greater than y")
+# elif x == y:
+#     print("x is equal to y")
+# else:
+#     print("y is greater than x")
+#
+# i = 1
+# while i < 10:
+#     print(i)
+#     i += 1
+#
+# fruits = ["apples", "bananna", "pear"]
+#
+# for fruit in fruits:
+#     print(fruit)
+
+x = 5
+
+print(not(x < 6 and x < 10))
+print(x < 6 and x < 10)
